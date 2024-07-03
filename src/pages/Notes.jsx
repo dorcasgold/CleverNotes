83e056d64bf11fb4fdf09efbdc18b0a18d7cb5b9
odetayo dorcas <odetayodorcas123@gmail.com>
2024-07-03 14:03:08 +0100

    search function

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import { CiLight } from "react-icons/ci";
 import { FaSearch } from "react-icons/fa";
-import NoteItem from '../components/NoteItem'
+import NoteItem from '../components/NoteItem';
 import { MdNoteAdd } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 function Notes({ notes }) {
   // toggleTheme
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const [text, setText] = useState('');
+  const [filteredNotes, setFilteredNotes] = useState(notes);
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -21,7 +23,13 @@ function Notes({ notes }) {
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
-  // toggleTheme
+
+  const handleSearch = () => {
+    setFilteredNotes(notes.filter(note => note.title.toLowerCase().includes(text.toLowerCase())));
+  };
+
+  useEffect(handleSearch, [text, notes]);
+
   return (
     <div className='w-full bg-gray-400 flex justify-center h-screen'>
       <div className='bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark px-4 w-[390px] flex flex-col gap-5 transition-colors duration-300'>
@@ -34,22 +42,27 @@ function Notes({ notes }) {
           </div>
         </header>
         <section className="flex gap-3 items-center relative mt-2">
-          <input type="text" placeholder="Search notes..." className="px-4 py-3 w-full bg-box-light rounded-lg dark:bg-box-dark transition-colors duration-300" />
+          <input
+            type="text"
+            placeholder="Search notes..."
+            className="px-4 py-3 w-full bg-box-light rounded-lg dark:bg-box-dark transition-colors duration-300"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
           <FaSearch className=" absolute cursor-pointer right-4 text-gray-500" />
         </section>
         <section className="grid grid-cols-2 gap-2">
-          {
-            notes.map(note => (
-              <NoteItem key={note.id} note={note} />
-            ))
-          }
+          {filteredNotes.length === 0 && <p className="empty__notes">No notes found.</p>}
+          {filteredNotes.map(note => (
+            <NoteItem key={note.id} note={note} />
+          ))}
         </section>
-        <div className="bg-teal-500 text-white text-2xl w-10 flex justify-center py-2 fixed bottom-3 ml-40  rounded-md">
-          <Link to='/create-note' ><MdNoteAdd /></Link>
+        <div className="bg-teal-500 text-white text-2xl w-10 flex justify-center py-2 fixed bottom-3 ml-40 rounded-md">
+          <Link to='/create-note'><MdNoteAdd /></Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Notes
+export default Notes;
